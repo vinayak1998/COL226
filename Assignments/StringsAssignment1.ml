e749a72e0574fd6d8a5d12c386feae3ba4ccb64b
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2018-01-24 11:21:58 +0530

    Dead code removed

--- Assignments/StringsAssignment1.ml
+++ Assignments/StringsAssignment1.ml
@@ -51,42 +51,7 @@ Define in OCaml the following:
     replaces the letter at the n-th position of s with w.  [ Prove that lgh(replace w s) = lgh(s). ]
 *)
 
-(*
-type 'a str = Empty | Letter of 'a | Concat of ('a str)*('a str);;
-type 'a str = Empty | Letter of 'a | Concat of 'a str * 'a str
 
-let rec lgh e = match e with
-    Empty -> 0
-  | Letter n -> 1
-  | Concat(e1,e2) -> (lgh e1) + (lgh e2);;
-
-let nonempty e = match e with
-    Empty ->  false
-  | _ -> true;;
-
-let concat e = match e with
-    (e1,e2) -> e1^e2;;
-
-let reverse s =
-    let rec helper i =
-        if i >= String.length s then "" else (helper (i+1))^(String.make 1 s.[i])
-    in
-        helper 0;;
-
-exception EmptyString;;
-
-let first e = match e with
-    "" -> raise EmptyString
-  | _ -> String.get e 0;;
-
-let last e = match e with
-    "" -> raise EmptyString
-  | _ -> String.get e ((String.length e)-1);;
-
-
-__________________________________________________________________________________________
-
-*)
 
 exception AtLast;;
 exception AtFirst;;
